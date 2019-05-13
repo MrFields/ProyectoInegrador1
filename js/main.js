d4aab8dceb927c557cad492820d4e2bd904e2356
Eduardo Campos <campos102010@gmail.com>
2019-05-13 18:01:47 -0500

    Estoy haciendo el formulario me falata terminarlo al 100 V1

---
+++ js/main.js
@@ -0,0 +1,76 @@
+function CE()
+{
+   $('#archivos').html(
+      '<h3>Favor de proporcionar la informacion siguiente</h3>'+
+      '<form action="mailto:dm.danmarlo@gmail.com" method="post" id="myform">'+
+     '<div class="form-group">' +
+          '<label class="negritas" for="exampleFormControlFile1"> •Acta constitutiva y sus modificaciones.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+         '</div>'+
+     '</div>'+
+     '<div class="form-group">'+
+          '<label class="negritas" for="exampleFormControlFile1"> •Instrumento público que contenga el poder donde conste las facultades del Representante legal de la empresa.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+      '</div>'+
+      '<div class="form-group">'+
+         ' <label class="negritas" for="exampleFormControlFile1"> •IFE del Rep. Legal de la empresa.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+     ' </div>'+
+      '<div class="form-group">'+
+         ' <label class="negritas" for="exampleFormControlFile1"> •Constancia de inscripción en el RFC.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+     '</div>'+
+     '<div class="form-group">'+
+         '<label class="negritas" for="exampleFormControlFile1"> •Constancia de la última declaración de impuestos.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+      '</div>'+
+      '<div class="form-group">'+
+         '<label class="negritas" for="exampleFormControlFile1"> •Comprobante de domicilio (menor de 30 días).</label>'+
+         '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+      '</div>'+
+      '<div class="form-group">'+
+          '<label class="negritas" for="exampleFormControlFile1"> •Lista de empleados y sus nacionalidades.</label>'+
+          '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+      '</div>'+
+      '<input type="submit" class="btn btn-info" href="javascript: submitform();">'+
+  '</form>'
+   );
+}
+function VTOT()
+{
+   $('#archivos').html(
+  '<h3>Favor de proporcionar la informacion siguiente</h3>'+
+  '<form action="mailto:dm.danmarlo@gmail.com" method="post" id="myform">'+
+ '<div class="form-group">' +
+      '<label class="negritas" for="exampleFormControlFile1"> •Constancia de Empleador.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+     '</div>'+
+ '</div>'+
+ '<div class="form-group">'+
+      '<label class="negritas" for="exampleFormControlFile1"> •Copia de pasaporte vigente.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+  '</div>'+
+  '<div class="form-group">'+
+     ' <label class="negritas" for="exampleFormControlFile1"> •Actividad laboral para desarrollar y nombre del puesto.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+ ' </div>'+
+  '<div class="form-group">'+
+     ' <label class="negritas" for="exampleFormControlFile1"> •Temporalidad que se oferta el trabajo al extranjero.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+ '</div>'+
+ '<div class="form-group">'+
+     '<label class="negritas" for="exampleFormControlFile1"> •Lugar de trabajo.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+  '</div>'+
+  '<div class="form-group">'+
+     '<label class="negritas" for="exampleFormControlFile1"> •Quien tendrá la responsabilidad de financiar el viaje de la persona extranjera (empresa o el extranjero).</label>'+
+     '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+  '</div>'+
+  '<div class="form-group">'+
+      '<label class="negritas" for="exampleFormControlFile1"> •Lista de empleados y sus nacionalidades.</label>'+
+      '<input type="file" class="form-control-file" id="exampleFormControlFile1">'+
+  '</div>'+
+  '<input type="submit" class="btn btn-info" href="javascript: submitform();">'+
+'</form>'
+   );
+}
