d68ebd6a6c2d9ccdc0bf0bff4d286681da9e1ddd
Daniel Martinez <dm.danmarlo@gmail.com>
2019-06-02 23:00:50 -0500

    Formulario de correo funcional

--- js/email.js
+++ js/email.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
    $('#submit').click(function(event){
-    event.preventDefault();
-    console.log('Le picaste al boton')
+
 
     var email = $('#email').val();
     var nombre = $('#nombre').val();
     var asunto = $('#asunto').val();
     var mensaje = $('#mensaje').val();
-    var status
+    var estatusElm = $('#asunto');
+    estatusElm.empty();
 
     if(email.length > 5 && email.includes('@') && email.includes('.'))
     {
-        console.log('Email Valido');
+        estatusElm.append('<div>Email valido</div>');
     } else {
-        console.log('Email invalido');
+        event.preventDefault();
+        estatusElm.append('<div>Email invalido</div>');
+    }
+
+    if(asunto.length > 2){
+        estatusElm.append('<div>Asunto valido</div>');
+    }else{
+        event.preventDefault();
+        estatusElm.append('<div>Asunto invalido</div>');
+    }
+
+    if(mensaje.length > 20){
+        estatusElm.append('<div>Mensaje valido</div>');
+    }else{
+        event.preventDefault();
+        estatusElm.append('<div>Mensaje invalido</div>');
     }
    }); 
 
